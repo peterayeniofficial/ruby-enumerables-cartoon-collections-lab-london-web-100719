98d60ba013ad65f74bac34538267eba57a6ac614
Peter Ayeni <ayenicology@gmail.com>
2019-09-26 18:41:07 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,6 +1,6 @@
 def roll_call_dwarves (array)# code an argument here
   # Your code here
-  array.each_with_index {|x, |
+  array.each_with_index {|x, i| puts i }
 end
 
 def summon_captain_planet# code an argument here
