230ef033284d8b549c026073e623d96c88db04c7
Peter Ayeni <ayenicology@gmail.com>
2019-09-26 21:28:30 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -15,7 +15,8 @@ def long_planeteer_calls (word_arrays) # code an argument here
   
 end
 
-def find_the_cheese# code an argument here
+def find_the_cheese (snacks) # code an argument here
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
+  
 end
