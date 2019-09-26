70d49c1609d455c019b4d4e95e816a0a758a7d61
Peter Ayeni <ayenicology@gmail.com>
2019-09-26 19:05:54 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -5,7 +5,7 @@ end
 
 def summon_captain_planet (planeteer)# code an argument here
   # Your code here
-  planeteer.collect {|x| puts "#{x}!".capitalize}
+  planeteer.collect {|x| "#{x}!".capitalize}
 end
 
 def long_planeteer_calls# code an argument here
