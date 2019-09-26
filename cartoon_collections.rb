4181cc077a092fb1ab0ffbce97324bf3482f8fc8
Peter Ayeni <ayenicology@gmail.com>
2019-09-26 23:00:48 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -20,7 +20,7 @@ def find_the_cheese (snacks) # code an argument here
   cheese_types = ["cheddar", "gouda", "camembert"]
   snacks.each_index do |index|
     if cheese_types.include?(snacks[index]) == true
-        snacks[index]
+        return snacks[index]
     end
   end
 end
